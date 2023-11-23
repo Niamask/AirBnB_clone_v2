551c2f49e96ef8ebd996d02d88bf46c77874d9cb
Niamask <niamaskay@gmail.com>
2023-11-23 03:26:23 -0800

    3. Python is cool!

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""A script that starts a flask web application
+Your web application must be listening on 0.0.0.0, port 5000
+/: display “Hello HBNB!”
+/hbnb: display “HBNB”
+/c/<text>: display “C ” followed by the value of the text variable
+/python/<text>: display “Python ”, followed by the value of the text
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/", strict_slashes=False)
+def index():
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def C_text(text):
+    # Replace underscores with spaces
+    text = text.replace('_',' ')
+
+    # Return the modified text with a prefix
+    return "C "+ text
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def Python_text(text = "is cool"):
+    # Replace underscores with spaces
+    text = text.replace('_',' ')
+
+    # Return the modified text with a prefix
+    return "Python "+ text
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=None)
+
