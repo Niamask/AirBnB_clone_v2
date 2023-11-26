5bb357bae7ef93ae96f9165f279743107b707ff8
Niamask <niamaskay@gmail.com>
2023-11-26 13:41:56 -0800

    update

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -8,25 +8,26 @@ Your web application must be listening on 0.0.0.0, port 5000
 
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask("__name__")
 
-@app.route("/", strict_slashes=False)
-def index():
-    return "Hello HBNB!"
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Return a given string"""
+    return ("Hello HBNB!")
+
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    return "HBNB"
+    """Returns a given string"""
+    return ("HBNB")
+
 
 @app.route("/c/<text>", strict_slashes=False)
-def C_text(text):
-    # Replace underscores with spaces
-    text = text.replace('_',' ')
-
-    # Return the modified text with a prefix
-    return "C "+ text
+def cText(text):
+    """display C followed by the value of the text variable"""
+    return "C {}".format(text.replace("_", " "))
 
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=None)
-
