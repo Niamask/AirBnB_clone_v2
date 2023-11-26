f05ddba2a287aa1414bd42b0f55e4f467b9746cf
Niamask <niamaskay@gmail.com>
2023-11-26 13:38:53 -0800

    0. Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,11 +6,14 @@ Your web application must be listening on 0.0.0.0, port 5000
 
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask("__name__")
 
-@app.route("/", strict_slashes=False)
-def index():
-    return "Hello HBNB!"
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Return a given string"""
+    return ("Hello HBNB!")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=None)
